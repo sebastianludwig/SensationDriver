da2b14984ec5bbe4f5ed3a0c1995f4df27b265c8
Sebastian Ludwig <sebastian@lurado.de>
2014-04-18 21:33:34 +0200

    Cleanup

--- sensationserver.py
+++ sensationserver.py
@@ -31,6 +31,8 @@ class SensationServer:
             buffer = buffer[4:]
           # message to parse
           elif message_size and len(buffer) >= message_size:
+            message = buffer[:message_size]
+            print 'received "%s"' % message
             buffer = buffer[message_size:]
             message_size = None
           # neither -> need more data
@@ -63,40 +65,3 @@ class SensationServer:
 server = SensationServer()
 server.listen('localhost', 10000)
 server.loop()
-
-exit()
-#----------------------------------
-    
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Bind the socket to the port
-server_address = ('localhost', 10000)
-print 'starting up on %s port %s' % server_address
-sock.bind(server_address)
-
-# Listen for incoming connections
-sock.listen(1)
-
-while True:
-  # Wait for a connection
-  print 'waiting for a connection'
-  connection, client_address = sock.accept()
-    
-  try:
-    print 'connection from', client_address
-
-    # Receive the data in small chunks and retransmit it
-    while True:
-      data = connection.recv(16)
-      print 'received "%s"' % data
-      if data:
-        print 'sending data back to the client'
-        connection.sendall(data)
-      else:
-        print 'no more data from', client_address
-        break
-        
-  finally:
-    # Clean up the connection
-    connection.close()
