e85b743a39c3a4016aee3d93dd6bb0a45daeaea0
Sebastian Ludwig <sebastian@lurado.de>
2014-06-19 16:32:31 +0200

    Enabled two important lines :-/

--- server.py
+++ server.py
@@ -18,8 +18,8 @@ if is_raspberry():
   reset_pin.high
 
   server.handler = messagehandler.MessageHandler()
-  # server.on_client_connect = reset_pin.low
-  # server.on_client_disconnect = reset_pin.high
+  server.on_client_connect = reset_pin.low
+  server.on_client_disconnect = reset_pin.high
 else:
   import messagelogger
   server.handler = messagelogger.MessageLogger()
