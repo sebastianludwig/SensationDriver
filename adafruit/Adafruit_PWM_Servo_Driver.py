202793579d10e096be41ce8a91c5963174197032
Sebastian Ludwig <sebastian@lurado.de>
2014-06-21 11:27:02 +0200

    Added wakeUp() and sleep() methods to servo driver

--- adafruit/Adafruit_PWM_Servo_Driver.py
+++ adafruit/Adafruit_PWM_Servo_Driver.py
@@ -50,10 +50,18 @@ class Adafruit_PWM_Servo_Driver:
     self.i2c.write8(self.__MODE2, self.__OUTDRV)
     self.i2c.write8(self.__MODE1, self.__ALLCALL)
     time.sleep(0.005)                                       # wait for oscillator
+    
+  def wakeUp(self):
+    "Activates the sleep mode"
     mode1 = self.i2c.readU8(self.__MODE1)
-    mode1 = mode1 & ~self.__SLEEP                 # wake up (reset sleep)
+    mode1 = mode1 & ~self.__SLEEP
     self.i2c.write8(self.__MODE1, mode1)
-    time.sleep(0.005)                             # wait for oscillator
+    time.sleep(0.005)
+
+  def sleep(self):
+    "Deactivates the sleep mode"
+    mode1 = self.i2c.readU8(self.__MODE1)
+    self.i2c.write8(self.__MODE1, mode1 | self.__SLEEP)
 
   def setPWMFreq(self, freq):
     "Sets the PWM frequency"
