bc13126cfe343164151e29cd7e1d99db585d2459
Sebastian Ludwig <sebastian@lurado.de>
2014-08-14 17:00:44 +0200

    Client runs in continues mode

--- bin/run-client.py
+++ bin/run-client.py
@@ -4,31 +4,36 @@ import project
 import sys
 sys.path.append(project.relative_path('src'))
 
+import time
+
 import sensationdriver
 from sensationdriver import protocol
 
-if len(sys.argv) < 5:
-    raise IndexError("Not enough parameters. Usage: run-client.py <host:string> <region:int> <actor:int> <intensity:float:[0-1]> [<priority:int>]")
+if len(sys.argv) < 4:
+    raise IndexError("Not enough parameters. Usage: run-client.py <host:string> <region:int> <actor:int> [<priority:int>]")
 
 
 vibration = protocol.Vibration()
 vibration.target_region = int(sys.argv[2])
 vibration.actor_index = int(sys.argv[3])
-vibration.intensity = float(sys.argv[4])
-if len(sys.argv) >= 6:
-    vibration.priority = int(sys.argv[5])
+if len(sys.argv) >= 5:
+    vibration.priority = int(sys.argv[4])
 
 
 message = protocol.Message()
 message.type = protocol.Message.VIBRATION
-message.vibration.CopyFrom(vibration)
-
 
 
 client = sensationdriver.Client()
 client.connect(sys.argv[1], 10000)
 
-client.send(message.SerializeToString())
-
+print("Intensity: ", end="", flush=True)
+for line in sys.stdin:
+    vibration.intensity = float(line)
+    message.vibration.CopyFrom(vibration)
+    client.send(message.SerializeToString())
+    print("Intensity: ", end="", flush=True)
 
 client.disconnect()
+
+print("")
