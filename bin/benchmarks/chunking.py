674ece838216a9591463aaa1d0025ee662dba81d
Sebastian Ludwig <sebastian@lurado.de>
2014-11-30 17:38:29 +0100

    Extended chunking benchmark.

--- bin/benchmarks/chunking.py
+++ bin/benchmarks/chunking.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python3.4
 
-import project
 import time
-import asyncio
-import signal
-import fileinput
 import line_profiler
-
-import sys
-sys.path.append(project.relative_path('src'))
 
 # @profile
 def do_stuff():
@@ -71,5 +64,75 @@ def do_stuff2():
     duration = (time.time() - start) * 1000
     print("reached %d in %.0f ms" % (counter, duration))
 
+# do_stuff2()
 
-do_stuff2()
+class Splitter():
+    def __init__(self):
+        self.buffer = bytes()
+        self.buffer_length = 0
+        self.start_index = 0
+        self.message_size = None
+
+        # only for profiling
+        self.counter = 0
+        self.start = None
+        self.message_counter = 0
+
+    # @profile
+    def process(self, new_data):
+        # This could be written much shorter, but the Pi performance is very deficient. 
+        # The used operations are chosen considerately.
+
+        new_data_length = len(new_data)
+
+        buffer_length = self.buffer_length
+        start_index = self.start_index
+        message_size = self.message_size
+
+        # profiling only
+        if self.start is None:
+            self.start = time.time()
+        self.counter += new_data_length
+
+        buffer = self.buffer[start_index:buffer_length] + new_data
+        buffer_length = buffer_length - start_index + new_data_length
+        start_index = 0
+
+        messages = []
+
+        # parse everything we received
+        while True:
+            if message_size is None and buffer_length - start_index >= 4:            # message_size to parse
+                message_size = int.from_bytes(buffer[start_index:start_index + 4], byteorder='big')
+                start_index += 4
+                message_size = 15
+            elif message_size and buffer_length - start_index >= message_size:      # message to parse
+                message = buffer[start_index:start_index + message_size]
+                # if self.handler is not None:
+                #     yield from self.handler.process(message)
+                messages.append(message)
+                start_index += message_size
+                message_size = None
+            else:        # neither -> need more data
+                break
+
+        self.message_counter += len(messages)
+
+        if self.counter >= 95000:
+            duration = (time.time() - self.start) * 1000
+            print("received %d bytes (%d msg) in %.0f ms" % (self.counter, self.message_counter, duration))
+            self.counter = 0
+            self.start = None
+
+        self.buffer_length = buffer_length
+        self.start_index = start_index
+        self.message_size = message_size
+        self.buffer = buffer
+
+        return messages
+
+data = bytearray([i % 255 for i in range(0, 53 * 25)])
+splitter = Splitter()
+for run in range(0, 95):
+    splitter.process(data)
+
